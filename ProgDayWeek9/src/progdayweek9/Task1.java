e5553974006ca3b70d7750e2559df07e20bd57ce
Aiemenaltaf <altafchandz1@gmail.com>
2021-04-25 14:26:41 +0500

    Task1,Task3,Task4
    String Manipulation Manual

--- ProgDayWeek9/src/progdayweek9/Task1.java
+++ ProgDayWeek9/src/progdayweek9/Task1.java
@@ -4,14 +4,75 @@
  * and open the template in the editor.
  */
 package progdayweek9;
+import java.util.Scanner;
 
 public class Task1 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     public static void main(String[] args) {
+        System.out.println("Enter String :");
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        
+        int a = wordCounter(str);
+        System.out.println("Number of Words :"+a);
+        
+        int b = vowelCounter(str);
+        System.out.println("Number of Vowels: "+b);
+        
+        int c = puncCount(str);
+         System.out.println("Number of Punctuation :"+c);
+     }
+     
+     public static int wordCounter(String s){
+        int count = 0;
+        char []ch = s.toCharArray();
+        for(int i=0; i < ch.length; i++){
+        if((ch[i] >= 'a' && ch[i] <= 'z')||( ch[i] >= 'A' && ch[i] <= 'Z') ||( ch[i] == ' ')){
+            count++;
+        }
+        }
+        return count;
+        
     }
-    
+     
+    public static int vowelCounter(String s){
+        int count = 0;
+        char []ch = s.toCharArray();
+        for(char c : ch){
+            switch(c){
+                case'a':
+                case'A':
+                case'e':
+                case'E':
+                case'i':
+                case'I':
+                case'o':
+                case'O':
+                case'u':
+                case'U':
+                count++;
+                  break;
+              
+          }
+        }
+        
+        return count;
+        
+    }
+    public static int puncCount(String s){
+        int count = 0;
+        char []ch = s.toCharArray();
+        for(int i=0; i < ch.length; i++){
+        if((ch[i] == '.') || (ch[i] == ',') || (ch[i] == ';') || (ch[i] == ':') 
+           || (ch[i] == '!') || (ch[i] == '&') || (ch[i] == '-') || (ch[i] == '"')
+           || (ch[i] == '?')){
+            count++;
+        }
+        }
+        return count;
+        
+    }
 }
